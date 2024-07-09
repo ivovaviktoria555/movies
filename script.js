714ddc6c1ebe82827fa95eb1e2e3eef960288630
ivovaviktoria555 <ivovaviktoria159@gmail.com>
2024-07-09 14:45:30 +0300

    commit1

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-const movies = fetch("http://localhost:3000/movies",)
+console.log
+("Hello");
+
+let is_clicked = false;
+
+function myFunction() {
+    let clickButtonElement = document.getElementById('click-button')
+    let buttonsDivElement = document.getElementById('buttons')
+
+    if (!is_clicked) {
+        let newButtonElement = document.createElement('button')
+        newButtonElement.classList.add('click-buttons')
+        newButtonElement.textContent = 'Click Me'
+
+        newButtonElement.addEventListener('click', () => {
+            is_clicked = false;
+            newButtonElement.remove()
+        })
+
+        buttonsDivElement.appendChild(newButtonElement)
+        is_clicked = true;
+    }
+  }
+;
+
